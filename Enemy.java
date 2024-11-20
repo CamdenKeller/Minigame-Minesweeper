6d0009300b1719d9731d09549ef3a1ca1e4514a7
Camden Keller <ckellersb@gmail.com>
2024-11-19 23:27:48 -0500

    Create Enemy.java

---
+++ Enemy.java
@@ -0,0 +1,26 @@
+public class Enemy {
+
+public Enemy(){
+    
+}
+
+public void move(Player a){
+
+    }
+    
+public String getName(){
+    return "";
+}
+
+public void loseHp(int x){
+    
+}
+
+public int getHp(){
+    return 0;
+    }
+
+public void showHp(){
+    }
+
+}
